f43b97096f2258bf2e999fcc06f1da2fee1b4ef2
Adriano Santos <54894816+adrianosantosgit@users.noreply.github.com>
2024-03-09 11:20:03 -0300

    Add files via upload

---
+++ lightanddarkmode/script.js
@@ -0,0 +1,8 @@
+function lightMode() {
+    const el = document.querySelector("link");
+    el.href = "light.css";
+}
+function darkMode() {
+    const el = document.querySelector("link");
+    el.href = "dark.css";
+}
